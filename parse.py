16cf5683e4d1f9e420aa14b2b51604fff5afc578
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2017-10-29 02:40:43 +0100

    z watkowaniem

--- parse.py
+++ parse.py
@@ -6,6 +6,8 @@ log_file.write("Zaczynam prace o: {date}\n".format(date=time_start))
 log_file.flush()
 import csv
 import re
+import time
+from multiprocessing import Process, Queue
 e = []
 def prepare(s):
     return s.lower().translate(str.maketrans("ęóąśłżźćńö","eoaslzzcno")).replace('  ', ' ').strip()
@@ -16,6 +18,20 @@ def ulica_prepare(s):
 def numer_prepare(s):
     return re.sub(" ", "", s).split("/")[0].split("-")[0]
 #    return s.replace("ulica", "").replace("al.","").replace("gen.","").replace("aleja", "").replace("os. ","").replace("osiedle ","").strip()
+
+def process(data):
+    for d in enumerate(data):
+        match = False
+        for s in ulice:
+            if (s[1] == d[1][1] and (s[0] in d[1][0])):
+                match = True
+                dane_raw[d[0]].append(s[2])
+                dane_raw[d[0]].append(s[3])
+                matched.put(dane_raw[d[0]])
+                break
+        if not match:
+            not_matched.put(dane_raw[d[0]])
+    return 0
 
 with open("slownik.csv", "r") as f:
     ulice = [(prepare(x[5]),numer_prepare(prepare(x[1])),x[10], x[11]) for x in csv.reader(f, delimiter=";")]
@@ -38,50 +54,58 @@ with open("dane.csv", "r") as f:
 log_file.write("Mam {dane} danych wejsciowych\n".format(dane=len(dane)))
 log_file.flush()
 
-matched = []
-not_matched = []
+matched = Queue()
+not_matched = Queue()
 
-for d in enumerate(dane):
-    match = False
-#    debug = []
-#    debug.append("d: {a} {b}".format(a=d[0], b=d[1])) 
-    #print("d:", d[0], d[1]) 
-    for s in ulice:
-#        debug.append("s:{a} {b}".format(a=s[0], b=s[1]))
-        #print("s:", s[0], s[1])
-        if (s[1] == d[1][1] and (s[0] in d[1][0])):
-#            debug.append("match")
-            #print("match")
-            match = True
-            dane_raw[d[0]].append(s[2])
-            dane_raw[d[0]].append(s[3])
-            matched.append(dane_raw[d[0]])
-            break
-    #print("---", match)
-    if not match:
-#        print("\n".join(debug))
-        not_matched.append(dane_raw[d[0]])
+l = len(dane)
+threads_num = 8
+split_num = round(l/threads_num + 0.5)
+threads = []
 
-log_file.write("Matched: {match}\n".format(match=len(matched)))
-log_file.write("Not matched: {not_match}\n".format(not_match=len(not_matched)))
+#import threading
+for i in range(threads_num):
+    print(i*split_num, (i+1)*split_num)
+    t = Process(target=process, args=(dane[i*split_num:(i+1)*split_num],))
+    t.start()
+    threads.append(t)
+
+matched_list = []
+not_matched_list = []
+
+allExited = False
+while not allExited:
+    while not matched.empty():
+        matched_list.append(matched.get())
+    while not not_matched.empty():
+        not_matched_list.append(not_matched.get())
+    allExited = True
+    for i in threads:
+        print(i.exitcode)
+        if i.exitcode is None:
+            allExited = False
+    time.sleep(1)
+
+
+
+log_file.write("Matched: {match}\n".format(match=len(matched_list)))
+log_file.write("Not matched: {not_match}\n".format(not_match=len(not_matched_list)))
 log_file.flush()
 
 with open("out.csv","w") as f:
     writer = csv.writer(f, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.write(dane_header+["X","Y"])
-    for r in matched:
+    writer.writerow(dane_header+["X","Y"])
+    for r in matched_list:
         writer.writerow(r)
 
 with open("bad.csv","w") as f:
     writer = csv.writer(f, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.write(dane_header)
-    for r in not_matched:
+    writer.writerow(dane_header)
+    for r in not_matched_list:
         writer.writerow(r)
 
 time_stop = datetime.datetime.now()
 log_file.write("Koncze prace o: {date}\n".format(date=time_start))
 log_file.write("Pracowalem: {czas}\n".format(czas=time_stop-time_start))
 log_file.flush()
-
 
 log_file.close()
